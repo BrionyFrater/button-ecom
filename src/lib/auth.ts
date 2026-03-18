fa4b76ca718635069a0646c0bdd2309cac7cde54
BrionyFrater <briefrater@gmail.com>
2026-03-18 10:13:36 -0500

    install deps

---
+++ src/lib/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+// biome-ignore lint/performance/noNamespaceImport: needed for better auth config
+import * as schema from "./auth-schema";
+
+const db = drizzle(new Pool({ connectionString: process.env.DATABASE_URL }), {
+  schema,
+});
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, { provider: "pg", schema }),
+  baseURL: "http://localhost:3000/",
+  emailAndPassword: { enabled: true },
+});
